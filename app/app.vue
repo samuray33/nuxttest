b5cb085c2ae83a34fa92c243a6a6db9a81b4de4c
Nekruz <nekruzh9@gmail.com>
2026-03-26 01:10:52 +0300

    сделал роутинг учебный

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,9 @@
+<!-- npx nuxi@latest init . -->
 <template>
-  <div>
-    
-  </div>
+  <!-- чуть по пожа вынесу это все в отдельный файл пока так -->
+  <NuxtLink to="/info">перейти в info </NuxtLink> <br />
+  <NuxtLink to="/"> Главное</NuxtLink>
+
+  <NuxtPage />
+  
 </template>
