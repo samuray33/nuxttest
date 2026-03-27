377d7e8dc5005f193585f0c3a47a3eb351a123d8
Nekruz <nekruzh9@gmail.com>
2026-03-28 02:50:05 +0300

    - useFetch - pending - загрузка - refresh - запуск - immediate - запретить загружать запрос при рендере - method: get/post - разобрался как указывать порты для сервера для страницы и тд

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,5 @@
 <!-- npx nuxi@latest init . -->
+ <!-- npx json-server db.json --port 4000 -->
 <template>
   <!-- чуть по пожа вынесу это все в отдельный файл пока так -->
   <NuxtLink to="/info">перейти в info </NuxtLink> <br />
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,44 @@
-<script setup lang="ts">
+<script setup>
+const rout = useRouter();
 
+// добавление данных
+// let user = {
+//   name: "name",
+//   surname: "surname"
+// }
+// let {data, pending: pendingPost, refresh: refreshPost} = await useFetch("http://localhost:4000/posts", {
+//   // что бы запрос не проийсходл пока я не укажу refreshPost
+//   immediate: false,
+//   method: "post",
+//   body: user
+// });
+// добавление данных через resreshPost
+// let dovnloade = async() => {
+//   await refreshPost();
+// }
+
+// получение данных
+let {data, pending: pendingPost, refresh: refreshPost} = await useFetch("http://localhost:4000/posts", {
+  immediate: false,
+  method: "get",
+});
+
+// загрузка данных через resreshPost
+let dovnloade = async() => {
+  await refreshPost();
+}
 
 </script>
 
 <template>
   <h1>Главная страница</h1>
   <Comp name = "Props TESTCOMP"/>
+  <!-- вывод поста из апи -->
+   <button @click="dovnloade">загрузить из апи</button>
+  <h1 v-if="pendingPost">загрузка.....</h1>
+  <div v-else v-for="post in data" :key="post.id">
+    <h1>{{ post.name }} {{ post.surname }}</h1>
+  </div>
 </template>
 
 <style scoped>
